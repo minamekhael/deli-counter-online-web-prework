682381a6ba58f8362e18f0553cad1be30b973ebe
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-07 20:55:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ def now_serving(line)
   if line.length == 0 
     puts "There is nobody waiting to be served!"
   else 
-    puts "Currently serving #{line}!"
+    puts "Currently serving #{line[0]}!"
     line.shift
 end
 end
