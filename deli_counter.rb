ff22ca350d465294bc8f6e1cc11c54eeca49111a
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-07 21:04:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,5 @@
 def line(array)
+  # Created an empty array and start counter for people in line. Display place in line and name. With the ampty array I joined If array.length is 0 displays different message.
   if array.length >= 1
      emptyarray = []
      counter = 1 
@@ -13,11 +14,13 @@ def line(array)
 end
 
 def take_a_number(line, new_person)
+  # calls on line method, and new_person to display name and number in line 
   line.push new_person
   puts "Welcome, #{new_person}. You are number #{line.length} in line."
 end 
 
 def now_serving(line)
+  # calls on line method to serve the next customer
   if line.length == 0 
     puts "There is nobody waiting to be served!"
   else 
