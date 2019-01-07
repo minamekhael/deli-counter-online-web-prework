89b72d07f8de82910e694af1220fd30fafc8149a
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-07 20:50:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,5 +17,6 @@ def take_a_number(line, new_person)
   puts "Welcome, #{new_person}. You are number #{line.length} in line."
 end 
 
-def now_serving
+def now_serving(line, take_a_number)
+  puts 
 end
