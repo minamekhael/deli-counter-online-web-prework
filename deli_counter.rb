ff7aa14076cb4dedfefc5303989786a2a4400ea8
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-07 20:43:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def line(array)
+  if array.length >= 1
+     emptyarray = []
+     counter = 1 
+     array.each do |name|
+     emptyarray.push("#{counter}. #{name}")
+     counter += 1 
+    end 
+    puts "The line is currently: #{emptyarray.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(katz_deli, new_person)
+end 
